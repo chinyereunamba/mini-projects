e17eb81e71c98e12d96fceacbfb32f99dc25e048
Chinyere <chinyereunamba17@gmail.com>
2024-12-23 18:31:08 +0100

    just commit

--- base/models.py
+++ base/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -114,6 +115,22 @@ class BlogPost(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
     created_at = models.DateTimeField(_("Created"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated"), auto_now=True)
+    slug = models.SlugField(_("Slug"), max_length=255, blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if self.slug == None:
+            slug = slugify(self.title)
+
+            has_slug = BlogPost.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + "-" + str(count)
+                has_slug = BlogPost.objects.filter(slug=slug).exists()
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
